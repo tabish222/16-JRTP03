f929e9da1be7a993c8caa8ff49348b389060dcf1
Tabish <rizwantabish333@gmail.com>
2021-07-25 15:13:44 +0530

    first commit

---
+++ src/main/java/com/str/SpringBootApp01Application.java
@@ -0,0 +1,13 @@
+package com.str;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp01Application.class, args);
+	}
+
+}
